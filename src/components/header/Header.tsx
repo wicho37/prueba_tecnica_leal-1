a33ab507e219d85983d801e78e55521ed51b6b1b
wicho37 <willihernandezramirez@gmail.com>
2024-09-29 22:45:13 -0500

    se agregan algunas imgenes como el shopping cart y el input de busqueda de productos y se modifica styles.css

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,9 @@
 
 
 import styles from "./Header.module.css"
-import logo from "../../assets/bg-quotes.png"
+import logo from "../../assets/iconExito.svg"
+import cart from "../../assets/cart-shopping-solid.svg"
+import lupa from "../../assets/magnifying-glass-solid.svg"
 
 
 
@@ -11,8 +13,16 @@ const Navbar = () => {
     <div className={styles.content_nav}>
         <div>
             <img src={logo} className={styles.logo} alt="" />
+            <div className={styles.input}>
+              <input 
+                type="text"
+                placeholder="busca tu producto...."
+              />
+              <img src={lupa} className={styles.lupa}alt="" />
+            </div>
         </div>
         <div className={styles.ul_li}>
+                <img src={cart} className={styles.shopping_cart} alt="" />
                 <ul className={styles.list}>
                     <li>Feature</li>
                     <li>Team</li>
